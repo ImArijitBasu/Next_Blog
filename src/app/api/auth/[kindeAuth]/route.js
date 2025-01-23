79303fba482a1c227748882fc3c5cbcc640f2728
Arijit <arijitbasubd@gmail.com>
2025-01-23 17:00:24 +0600

    deploy error

--- src/app/api/auth/[kindeAuth]/route.js
+++ src/app/api/auth/[kindeAuth]/route.js
@@ -1,2 +1,10 @@
 import {handleAuth} from "@kinde-oss/kinde-auth-nextjs/server";
-export const GET = handleAuth();
+export const GET = handleAuth({
+    cookies: {
+      name: 'kinde_auth',
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      sameSite: 'lax',
+    },
+  });
+  
