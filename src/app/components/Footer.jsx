6ba6f0b9e4b7bca01b52211b81c7cc31752b9a79
Arijit <arijitbasubd@gmail.com>
2025-01-23 17:27:37 +0600

    deploy error

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-      <footer className="footer footer-center bg-slate-200 text-base-content p-4">
+      <footer className="footer mt-10 footer-center bg-slate-200 text-base-content p-4">
         <aside>
           <p>
             Copyright © {new Date().getFullYear()} - All right reserved by NEXT
--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,19 +5,7 @@ import { redirect } from "next/navigation";
 const Profile = async () => {
   const session = await getKindeServerSession();
   const { isAuthenticated } = session;
-  // const user = await getUser();
-  // console.log("User Data:", user);
 
-  // if (!user) {
-  //   console.log("User data is null despite being authenticated.");
-  //   return redirect('/api/auth/login');
-  // }
-
-  // return (
-  //   <div>
-  //     <h1 className="text-3xl font-bold text-center my-5">Welcome to your profile!</h1>
-  //   </div>
-  // );
   return (await isAuthenticated()) ? (
     <div>
       <h1 className="text-3xl font-bold text-center my-5">
