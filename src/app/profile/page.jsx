da9c834b5651670f28f087a12a05f4b2e1944526
Arijit <arijitbasubd@gmail.com>
2025-01-23 16:51:29 +0600

    deploy error

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,32 +1,36 @@
-
-import React from 'react';
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-import { redirect } from 'next/navigation';
+import React from "react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 const Profile = async () => {
   const session = await getKindeServerSession();
-  const { isAuthenticated, getUser } = session;
+  const { isAuthenticated } = session;
+  // const user = await getUser();
+  // console.log("User Data:", user);
 
-  // if (!isAuthenticated()) {
-  //   console.log("User is not authenticated. Redirecting to login...");
+  // if (!user) {
+  //   console.log("User data is null despite being authenticated.");
   //   return redirect('/api/auth/login');
   // }
-  console.log(isAuthenticated);
-  const user = await getUser();
-  console.log("User Data:", user);
 
-  if (!user) {
-    console.log("User data is null despite being authenticated.");
-    // return redirect('/api/auth/login');
-  }
-
-  return (
+  // return (
+  //   <div>
+  //     <h1 className="text-3xl font-bold text-center my-5">Welcome to your profile!</h1>
+  //   </div>
+  // );
+  return (await isAuthenticated()) ? (
     <div>
-      <h1 className="text-3xl font-bold text-center my-5">Welcome to your profile!</h1>
+      <h1 className="text-3xl font-bold text-center my-5">
+        Welcome to your profile!
+      </h1>
+    </div>
+  ) : (
+    <div>
+      <h1 className="text-3xl font-bold text-center my-5">
+        you are not authenticated
+      </h1>
     </div>
   );
 };
-
-
 
 export default Profile;
