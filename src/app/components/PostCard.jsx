0f57a7f354bacfa46b29ce8e0b3623cf93f52b1a
Arijit <arijitbasubd@gmail.com>
2025-01-23 18:18:54 +0600

    deploy error

--- src/app/components/PostCard.jsx
+++ src/app/components/PostCard.jsx
@@ -8,7 +8,7 @@ const PostCard = ({ id, title, body }) => {
       <div className="card bg-base-100 w-full shadow-xl">
         <div className="card-body">
           <h2 className="card-title">{title}</h2>
-          <p>{body}</p>
+          {/* <p>{body}</p> */}
           <div className="card-actions justify-end">
             <Link href={`/blogs/${id}`}>
               <button className="btn btn-primary">Details</button>
